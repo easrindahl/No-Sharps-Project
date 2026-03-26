f6c1a4143412292650d2cfccbbbd0c7c2dc98c87
Easton Rindahl <144266368+easrindahl@users.noreply.github.com>
2026-03-26 11:45:25 -0500

    initialized supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'navigation_shell.dart';
 
-void main() {
-  runApp(const MyApp());
+// void main() {
+//   runApp(const MyApp());
+// }
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); 
+
+  await Supabase.initialize(
+    url: 'https://sbqgcbiceleykldwzeas.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicWdjYmljZWxleWtsZHd6ZWFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMDA4MDksImV4cCI6MjA4OTg3NjgwOX0.HI2wdvj1wUxjSgdumTd91UB9mteM80Oah2ORDSMjQBs',
+  );
+  runApp(MyApp()); 
 }
 
 class MyApp extends StatelessWidget {
