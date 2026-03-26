a37ac69e54bfb61b14e46758a1c37a316874d155
Easton Rindahl <144266368+easrindahl@users.noreply.github.com>
2026-03-26 12:24:17 -0500

    Added report button and blank report page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'navigation_shell.dart';
+import 'views/report_view.dart';
 
 // void main() {
 //   runApp(const MyApp());
@@ -13,7 +14,7 @@ Future<void> main() async {
     url: 'https://sbqgcbiceleykldwzeas.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicWdjYmljZWxleWtsZHd6ZWFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMDA4MDksImV4cCI6MjA4OTg3NjgwOX0.HI2wdvj1wUxjSgdumTd91UB9mteM80Oah2ORDSMjQBs',
   );
-  runApp(MyApp()); 
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -26,6 +27,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
+      routes: {
+        '/report': (context) => const ReportView(),
+      },
       home: const NavigationShell(),
     );
   }
