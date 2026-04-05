3f72bee47913a3ac6856f9600f96c7481f782188
Mikey Petroff <petro258@d.umn.edu>
2026-04-05 00:03:17 -0500

    map

--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapView extends StatelessWidget {
+class MapView extends StatefulWidget {
   const MapView({super.key});
+
+  @override
+  State<MapView> createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  GoogleMapController? _mapController;
+
+  static const LatLng _initialPosition = LatLng(46.7834, -92.1006);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Map')),
-      body: Center(
-        child: Text(
-          'Map page',
-          style: Theme.of(context).textTheme.headlineSmall,
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
+          target: _initialPosition,
+          zoom: 15,
         ),
+        onMapCreated: (controller) {
+          _mapController = controller;
+        },
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
       ),
     );
   }
