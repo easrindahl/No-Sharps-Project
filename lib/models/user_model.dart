222606d5ac563417018f870858073b4e7df6ab0c
schmu110 <schmu110@d.umn.edu>
2026-04-08 20:08:53 -0500

    Added user rewards program and supabase configurations

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,11 +1,33 @@
 class UserModel {
   final String? id;
   final String? email;
+  final int reportCount;
+  final int pickupCount;
+  final int totalPoints;
 
   const UserModel({
     this.id,
     this.email,
+    this.reportCount = 0,
+    this.pickupCount = 0,
+    this.totalPoints = 0,
   });
 
   bool get isLoggedIn => id != null;
+
+  UserModel copyWith({
+    String? id,
+    String? email,
+    int? reportCount,
+    int? pickupCount,
+    int? totalPoints,
+  }) {
+    return UserModel(
+      id: id ?? this.id,
+      email: email ?? this.email,
+      reportCount: reportCount ?? this.reportCount,
+      pickupCount: pickupCount ?? this.pickupCount,
+      totalPoints: totalPoints ?? this.totalPoints,
+    );
+  }
 }
